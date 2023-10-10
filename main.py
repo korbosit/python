f3dc611e3bb46520c4af8e465d1ff06ca3d0dcc6
Vlaimir Korba <krissky1986@gmail.com>
2023-10-10 16:00:36 +0300

    4-function-practice

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-# a = {7, 8, 9, 10}
+# def merge_lists_to_dict(list_one, list_two):
+#     return dict(zip(list_one, list_two))
 
-# a.add(13)
 
-# b = {7, 13, 14, 15, 16}
+# res_one = merge_lists_to_dict(['a', 'b', 'c'], [10, True, []])
+# print(res_one)
 
-# c = (a | b) - (a & b)
+# res_two = merge_lists_to_dict(['abc'], [{}, True, 100])
+# print(res_two)
 
-# d = list(c)
+# res_three = merge_lists_to_dict(['a', True, 100], ['abc'])
+# print(res_three)
 
-# print(d)
 
 #  ========================================================================================================================================================
 
+# def merge_lists_to_dict(list_one, list_two):
+#     return dict(zip(list_one, list_two))
 
-set_one = {10, 5, 7, 15, 100}
 
-set_one.add(200)
+# res_one = merge_lists_to_dict(
+#     list_one=['a', 'b', 'c'], list_two=[10, True, []])
+# print(res_one)
 
-set_two = {20, 7, 300, 100, 200}
+# res_two = merge_lists_to_dict(list_two=[{}, True, 100], list_one=['abc'])
+# print(res_two)
 
-intersected_set = set_one.intersection(set_two)
-print(intersected_set)
+# res_three = merge_lists_to_dict(['a', True, 100], list_two=['abc'])
+# print(res_three)
 
-my_list = list(intersected_set)
-print(my_list)
+# # # Error - SyntaxError: positional argument follows keyword argument
+# # res_three = merge_lists_to_dict(list_two=['abc'], ['a', True, 100])
+# # print(res_three)
 
-print(set_one)
-print(set_two)
+
+#  ========================================================================================================================================================
+
+# def update_car_info(**car):
+#     car['is_available'] = True
+#     return car
+
+
+# print(update_car_info(brand='BMW', price=100000))
+
+# # # TypeError: update_car_info() takes 0 positional arguments but 2 were given
+# # print(update_car_info('BMW', 100000))

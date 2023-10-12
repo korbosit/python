60d911530fde224ca227ae13c623da3f133557df
Vlaimir Korba <krissky1986@gmail.com>
2023-10-12 12:38:39 +0300

    10-for-in-comprehensions

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-# while True:
-#     num_one = int(input("Enter first number: "))
-#     num_two = int(input("Enter second number: "))
-#     res = num_one / num_two
-#     print(f"The result of dividing the first number by the second: {res}")
-#     question = input("Do you want to continue?: ")
-#     if question == "yes":
-#         continue
-#     break
+# my_motorbike = {
+#     'brand': 'bmw',
+#     'country': 'germany',
+#     'owner': 'bogdan'
+# }
+
+# bike = {k: v.upper() for k, v in my_motorbike.items()}
+
+# print(bike)
+# print(my_motorbike)
 
 #  ========================================================================================================================================================
 
-while True:
-    try:
-        num_one = float(input("Please enter number one: "))
-        num_two = float(input("Please enter number two: "))
-    except ValueError as e:
-        print(e)
-        print("You must enter numbers!")
-        continue
+# my_list = ['Vova', 'Timka', 'Alfa', 'Beta', 'a', 'b', 'c', 'd', 'egd']
 
-    print(num_one / num_two)
+# new_my_list = [e for e in my_list if len(e) >= 3]
 
-    answer = input("Do you want to continue? (yes/no): ")
-    if answer == 'no':
-        break
+# print(new_my_list)
+
+#  ========================================================================================================================================================
+
+# my_motorbike = ['BMW', 'Germany', 'Bogdan']
+
+# bike = [el for el in my_motorbike if len(el) > 3]
+
+# print(bike)
+# print(my_motorbike)

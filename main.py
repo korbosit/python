c0ce0b6e0ea975b2be15a720cba59104426781d0
Vlaimir Korba <krissky1986@gmail.com>
2023-10-26 14:08:33 +0300

    12-json-practice

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-class Image:
-    def __init__(self, resolution, title, extension):
-        self.resolution = resolution
-        self.title = title
-        self.extension = extension
+import json
 
-    def resize(self, new_resolution):
-        self.resolution = new_resolution
+my_dict = {
+    'a': 10,
+    'b': {
+        'c': [1, 2, 3]
+    },
+    'd': (1, 2, 3)
+}
 
-    def __str__(self):
-        return f"{self.title}.{self.extension}"
+converted_dict = json.dumps(my_dict, indent=4)
+print(converted_dict)
+print(type(converted_dict))
 
-
-first_img = Image('1920x1080', "My dog", 'jpg')
-
-print(first_img.resolution)
-print(first_img.title)
-print(first_img.extension)
-
-first_img.resize('4000x3000')
-
-print(first_img.resolution)
-
-second_img = Image('8000x5000', "My cat", 'png')
-
-print(first_img)
+converted_json = json.loads(converted_dict)
+print(converted_json)
